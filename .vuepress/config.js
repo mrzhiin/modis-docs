548fd55832c7fa2f049a944513e4fa2e0d26e2bf
mrzhiin <mrzhiin@gmail.com>
2019-08-23 12:36:43 +0800

    Update

---
+++ .vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: "Modis",
+  base: "/",
+  head: [
+    [
+      "script",
+      {
+        src:
+          "https://cdn.jsdelivr.net/npm/@mrzhiin/modis@latest/dist/modis.min.js"
+      }
+    ],
+    [
+      "script",
+      {
+        src: "/main.js"
+      }
+    ]
+  ],
+  themeConfig: {
+    displayAllHeaders: true,
+    nav: [
+      { text: "指南", link: "/guide/" },
+      { text: "配置", link: "/config/" }
+    ],
+    sidebar: {
+      "/guide/": ["", "use", "backend"],
+      "/config/": ["", "methods"]
+    },
+    repo: "https://github.com/mrzhiin/modis"
+  }
+};
---
+++ .vuepress/public/main.js
@@ -0,0 +1,4 @@
+Modis.initAV({
+  appId: "vqUa4z3qMJp16I5wfYHXEt1x-MdYXbMMI",
+  appKey: "7vtNB5pARaTm5QEdhbQKCxOs"
+});
